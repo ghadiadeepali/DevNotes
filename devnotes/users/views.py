b1b9b00893bf3ebde4162d0c9c9631ccc764b90f
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-21 21:15:48 +0530

    created user_registration_view

--- devnotes/users/views.py
+++ devnotes/users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework.decorators import api_view
+from users import serializers
+from rest_framework.response import Response
+from rest_framework import status
 # Create your views here.
+
+@api_view(["POST"])
+def user_registration_view(request):
+
+    if request.method == "POST":
+            # check if both passwords match
+        password1 = request.data.get("password")
+        password2 = request.data.get("password2")
+        if password1 != password2:
+            return Response({"msg": "Passwords do not match. Please re-enter passwords"},status=status.HTTP_400_BAD_REQUEST )
+        serializer = serializers.UserRegistrationSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return serializer.data
