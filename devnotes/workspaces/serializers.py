fc2dc6ec7b2472169cd3a355e6a5238fbed4e37f
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-29 21:12:14 +0530

    created ListWorkspacesSerializer and CreateWorkspaceSerializer

---
+++ devnotes/workspaces/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from workspaces.models import Workspace
+
+class ListWorkspacesSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Workspace
+        fields = "__all__"
+        
+        
+class CreateWorkspaceSerializer(serializers.ModelSerializer):
+    description = serializers.CharField(required=False, allow_blank=True)
+    class Meta:
+        model = Workspace
+        fields = ["name", "description"]
