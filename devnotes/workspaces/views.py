4fff145bf46d679c9342be1403573a86e9c053f6
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-29 21:38:47 +0530

    defined function to list workspaces and create a workspace

--- devnotes/workspaces/views.py
+++ devnotes/workspaces/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+from workspaces.models import Workspace
+from workspaces.serializers import ListWorkspacesSerializer, CreateWorkspaceSerializer
+
 
 # Create your views here.
+@api_view(["GET"])
+def list_workspaces(request):
+    workspaces = Workspace.objects.all()
+    serializer = ListWorkspacesSerializer(workspaces, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
+
+@api_view(["POST"])
+def create_workspace(request):
+    serializer = CreateWorkspaceSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors)
