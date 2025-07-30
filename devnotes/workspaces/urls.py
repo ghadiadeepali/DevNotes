01adfd92dd39742fef4af578a88710f818c756a2
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-30 22:11:09 +0530

    created urlpattern to get details of a workspace

--- devnotes/workspaces/urls.py
+++ devnotes/workspaces/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from workspaces import views
 urlpatterns = [
     path("",views.list_workspaces),
-    path("create/", views.create_workspace)
+    path("create/", views.create_workspace),
+    path("<int:pk>", views.get_workspace_details)
 ]
