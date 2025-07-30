fa3eff10cb07814236284a0c33837696708e706f
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-30 22:22:03 +0530

    created urlpattern to update a workspace details

--- devnotes/workspaces/urls.py
+++ devnotes/workspaces/urls.py
@@ -3,5 +3,6 @@ from workspaces import views
 urlpatterns = [
     path("",views.list_workspaces),
     path("create/", views.create_workspace),
-    path("<int:pk>", views.get_workspace_details)
+    path("<int:pk>", views.get_workspace_details),
+    path("update/<int:pk>", views.update_a_workspace)
 ]
