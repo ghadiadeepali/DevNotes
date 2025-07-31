48003e1f5df5839af4b7dd9b0bcbed3f52477683
ghadiadeepali <deepalinghadia1606@gmail.com>
2025-07-31 15:09:54 +0530

    added ManyToMany Relationship in Workspace model for workspaces and collaborators

--- devnotes/workspaces/models.py
+++ devnotes/workspaces/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from users.models import CustomUser
 
 # Create your models here.
 class Workspace(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    collaborators = models.ManyToManyField(CustomUser)
